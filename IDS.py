d925e040d65ca5ccf483b6adedf0d35069584108
Matthew Carroll <matthew.j.carroll@outlook.com>
2018-09-10 21:52:37 -0500

    fixed read in with static testCases

--- IDS.py
+++ IDS.py
@@ -1,23 +1,27 @@
-fringe = []
-problem = [][]
 
-filename = input("Please enter the filename of the puzzle you wish to have solved...")
-problem = readInFile(filename)
-answer = treeSearch(problem, fringe)
-if(answer == -1):
-	print("Life is hard and the program failed. Sorry...")
-else:
-	print("Life is good! The program worked!")
+if __name__ == '__main__':
+
+    testCase1 = [[1,2,7,3],[5,6,11,4],[9,10,15,8],[13,14,12,0]]
+    testCase2 = [[5,1,7,3],[9,2,11,4],[13,6,15,8],[0,10,14,12]]
+	fringe = []
+
+	filename = input("Please enter the filename of the puzzle you wish to have solved...")
+	# problem = readInFile(filename)
+	answer = treeSearch(testCase1, fringe)
+	if(answer == -1):
+		print("Life is hard and the program failed. Sorry...")
+	else:
+		print("Life is good! The program worked!")
 
 
-def readInFile(filename):
-	problem = [][]
-	with open(filename, 'r') as f:
-		#so, theoretically, this loops thru trying to add values to problem[][] giving a 4x4 matrix
-		for x in xrange(1,4):
-			for y in xrange(1,4):
-				problem[x][y] = f.read()
-	return problem
+# def readInFile(filename):
+# 	problem = [][]
+# 	with open(filename, 'r') as f:
+# 		#so, theoretically, this loops thru trying to add values to problem[][] giving a 4x4 matrix
+# 		for x in xrange(1,4):
+# 			for y in xrange(1,4):
+# 				problem[x][y] = f.read()
+# 	return problem
 
 
 def treeSearch(problem, fringe):
