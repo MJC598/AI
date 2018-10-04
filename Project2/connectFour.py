fd7192c357f43fbeda3aacdf727de4a14689af44
Matthew Carroll <matthew.j.carroll@outlook.com>
2018-10-03 22:55:08 -0500

    please speed up

--- Project2/connectFour.py
+++ Project2/connectFour.py
@@ -4,7 +4,7 @@ from random import randint
 from heuristic import *
 
 #this takes the board and evaluates the heuristic to return to the minimax tree
-def minimax_exec(board, player):
+def minimax_exec(board, player, x_list, o_list, action_list):
     opponent_symbol = 'E'
     player_symbol = 'E'
     if player == 'p1':
@@ -15,50 +15,30 @@ def minimax_exec(board, player):
         opponent_symbol = 'X'
     #initialize return vars
     winner = None
-    #previous moves (list of coordinate pairs)
-    x_list = []
-    o_list = []
-    #possible moves (list of coordinate pairs)
-    action_list = []
-    #fill the lists with coordinate pairs of possible moves and all previous moves
-    for x in range(6):
-        for y in range(6):
-            if board[x][y] == 'E':
-                action_list.append(tuple((x,y)))
-            elif board[x][y] == 'X':
-                x_list.append(tuple((x,y)))
-                x_check_value = heuristic(x, y, board, 'X', 'O')
-                if(x_check_value == 1000000000):
-                    winner = 'p1'
-                    return tuple((winner, None))
-            else:
-                o_list.append(tuple((x,y)))
-                y_check_value = heuristic(x, y, board, 'O', 'X')
-                if(y_check_value == 1000000000):
-                    winner = 'p2'
-                    return tuple((winner, None))
     #verify there are still possible moves on the board
     if not action_list:
         winner = 'tie'
         return tuple((winner, None))
 
-    #sort each of the lists by the first number of the coordinate pair, it can make it easier to compare
-    x_list.sort(key=lambda tup: tup[0])
-    o_list.sort(key=lambda tup: tup[0])
-    action_list.sort(key=lambda tup: tup[0])
-
     h_value_list = []
-
     temp_board = deepcopy(board)
     print_board(board)
     # print_board(temp_board)
     #returns the max of the min value and the correct move
     choice = min_value(action_list, temp_board, player_symbol, opponent_symbol, 0)
+    if choice[0] == 1000000000:
+        winner = 'p1'
+    elif choice[0] == -1000000000:
+        winner = 'p2'
     move = choice[1]
     print_board(board)
 
     print(move)
-
+    action_list.remove(move)
+    if player_symbol == 'X':
+        x_list.push(move)
+    else:
+        o_list.push(move)
     #move is a coordinate pair tuple
     return tuple((winner,move))
 
@@ -132,9 +112,9 @@ def max_value(action_list, board, player_char, opp_char, increment):
 #call the heuristic to get the correct move and then execute it. Looks ahead 2 moves (1 for opp, 1 for me)
 #if the game is over, it returns the winner and the board (in a tuple)
 #otherwise, board and blank
-def minimax_tree(board, player):
+def minimax_tree(board, player, x_list, o_list, action_list):
     winner = None
-    winner, choice = minimax_exec(board, player)
+    winner, choice = minimax_exec(board, player, x_list, o_list, action_list)
     if choice is None:
         return tuple((winner, board))
     new_board = update_board(board, player, choice)
@@ -184,13 +164,18 @@ if __name__ == "__main__":
     offset1 = randint(0,1)
     offset2 = randint(0,1)
     board = update_board(new_board, player, (2+offset1,2+offset2))
+    action_list = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(2,0),(2,1),(2,2),(2,3),(2,4),
+        (2,5),(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(5,0),(5,1),(5,2),(5,3),(5,4),(5,5)]
+    action_list.remove((2+offset1,2+offset2))
+    x_list = []
+    o_list = []
     #run this 100 times to get new winners each time?
     #return p1, p2, or tie breaks the loop
     while winner is None:
         #check to see who played last, alternates between p1 and p2
         if(player == 'p1'):
             player = 'p2'
-            winner, board = minimax_tree(board, player)
+            winner, board = minimax_tree(board, player, x_list, o_list, action_list)
             if winner != None:
                 if winner == 'p2':
                     winner_list.append("p2")
@@ -200,7 +185,7 @@ if __name__ == "__main__":
                 break
         else:
             player = 'p1'
-            winner, board = minimax_tree(board, player)
+            winner, board = minimax_tree(board, player, x_list, o_list, action_list)
             if winner != None:
                 if winner == 'p1':
                     winner_list.append("p1")
