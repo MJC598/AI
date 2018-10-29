ed696f55d2722934918d8fc5730261fbb8dcbc2a
David Huangal <dbhuangal1204@gmail.com>
2018-10-29 01:49:27 -0500

    Implemented file_to_line_list function

--- Project3/backtracking.py
+++ Project3/backtracking.py
@@ -1,4 +1,24 @@
 from time import clock
 from copy import deepcopy
 
+def file_to_line_list(filename):
+    """
+    Creates a list of lines from the file, where each line is
+    a list of tokens split by whitespace.
+    e.g. the line "p cnf 3 5" would be added to the list as:
+    ['p', 'cnf', '3', '5']
+
+    returns list of lines
+    """
+    lines = []
+
+    with open(filename) as file:
+        for line in file:
+            # rstrip() removes the '\n' newline character
+            lines.append(line.rstrip().split())
+
+    return lines
+
+
 if __name__ == "__main__":
+    pass
