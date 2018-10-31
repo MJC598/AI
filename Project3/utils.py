f1051d973907f8776fe85742ad59e56ee79af2bc
David Huangal <dbhuangal1204@gmail.com>
2018-10-31 13:48:55 -0500

    Added utils.py file

---
+++ Project3/utils.py
@@ -0,0 +1,19 @@
+def argmin_random_tie(seq, key=identity):
+    """Return a minimum element that ties at random"""
+    return min(shuffled(seq), key=key)
+
+
+def count(seq):
+    """Count the number of items in sequence that are interpreted as true"""
+    return sum(bool(x) for x in seq)
+
+def first(iterable, default=None):
+    """Return the first element of an iterable or the next element of a generator or default."""
+    try:
+        return iterable[0]
+    except IndexError:
+        return default
+    except TypeError:
+        return next(iterable, default)
+
+
